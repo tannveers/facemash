af19307baedf9d3a7f0ae648f3e8556eb66931e8
Tanveer Singh <tanveer9812@gmail.com>
2020-08-01 17:14:28 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,8 @@ import 'firebase/storage';
 import 'firebase/firestore';
 
 var firebaseConfig = {
-  apiKey: 'AIzaSyCMWwTJTETsAWH-JkmgpgLGQ5vezUgfZ-8',
-  authDomain: 'facemash-d8ef4.firebaseapp.com',
-  databaseURL: 'https://facemash-d8ef4.firebaseio.com',
-  projectId: 'facemash-d8ef4',
-  storageBucket: 'facemash-d8ef4.appspot.com',
-  messagingSenderId: '45665864427',
-  appId: '1:45665864427:web:88ef219325d063b1330cae',
-};
+// use the firebase Config for your personal account
+}
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
